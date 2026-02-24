310e91c798965afd3ec16baa508c934426900be2
vinicius.vacare <vinicius.vacare@softplan.com>
2026-02-24 16:30:48 -0300

    Outras coisas

---
+++ src/generics/CaixaDeCoisas.java
@@ -0,0 +1,13 @@
+package generics;
+
+public class CaixaDeCoisas<T> {
+    private T conteudo;
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+}
